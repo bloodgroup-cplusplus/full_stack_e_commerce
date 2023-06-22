7dfe81bf203ee1a7a55d3126d0bc885ba8b8941f
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-22 10:43:06 +0530

    added bunch of stuff

--- ecommerce-app/sanity/schemas/product-schema.ts
+++ ecommerce-app/sanity/schemas/product-schema.ts
@@ -1,1 +1,47 @@
-export const product = {}
+import { defineField, defineType } from "sanity"
+
+export const product = {
+    name:"product",
+    title:"Products",
+    type:"document",
+
+
+    fields:[
+        defineField({
+            name:"name",
+            title:"Name",
+            type:"string"
+        }),
+        {
+            name:"slug",
+            title:"Slug",
+            type:"slug",
+            options:{
+                source:"name"
+            }
+        },
+        {
+            name:"images",
+            titleL:"Images",
+            type:"array",
+            of:[{type:"image"}]
+        },
+        {
+            name:"categories",
+            title:"Categories",
+            type:"array",
+            of:[{type:"string"}]
+
+        },
+        {
+            name:"colors",
+            title:"Colors",
+            type:"array",
+            of:[{type:"string"}]
+        }
+    ]
+
+
+
+
+}
