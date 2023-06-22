0ddb2716bcb3cf7fe428db67857a103cdf507000
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-22 04:46:01 +0000

    updated back to usd

--- ecommerce-app/components/providers.tsx
+++ ecommerce-app/components/providers.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 export function Providers({ children }: Props) {
   return <CartProvider 
-      currency="$"
+      currency="USD"
       shouldPersist
       cartMode="checkout-session"
       stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!}
